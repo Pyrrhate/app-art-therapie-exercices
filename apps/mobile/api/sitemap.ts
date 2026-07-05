168c17aa136995457a6d7d3e9e8fb9d545ccd566
Pyrrhate <guillaume.canva@gmail.com>
2026-07-05 10:38:16 +0200

    Fix sitemap statique pour Google Search Console

--- apps/mobile/api/sitemap.ts
+++
@@ -1,8 +0,0 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { buildSitemapXml } from "@/lib/seo/sitemap-urls";
-
-export default function handler(_req: VercelRequest, res: VercelResponse) {
-  res.setHeader("Content-Type", "text/xml; charset=utf-8");
-  res.setHeader("Cache-Control", "public, max-age=3600, s-maxage=86400");
-  res.status(200).send(buildSitemapXml());
-}
